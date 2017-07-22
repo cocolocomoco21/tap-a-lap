1c30f94817ae6fc4d8bc173b7575a9252ee83e53
Matt Henricks <mchenricks@wisc.edu>
2017-07-22 18:36:16 -0500

    Move SharedPreferenceChangeListener from settings fragment to activity

--- app/src/main/java/com/cocolocomoco/tapalap/SettingsActivity.java
+++ app/src/main/java/com/cocolocomoco/tapalap/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.cocolocomoco.tapalap;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity  implements SharedPreferences.OnSharedPreferenceChangeListener {
 	protected static final String KEY_PREF_SCREEN_ON = "pref_screen_on";
 
 	@Override
@@ -14,5 +17,37 @@ public class SettingsActivity extends AppCompatActivity {
 		getFragmentManager().beginTransaction()
 				.add(android.R.id.content, new SettingsFragment())
 				.commit();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		// Register listener for preference change
+		PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		// Unregister listener for preference change
+		PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if (key.equals(SettingsActivity.KEY_PREF_SCREEN_ON)) {
+			boolean result = sharedPreferences.getBoolean(key, true);
+			onUpdateScreenOn(result);
+		}
+	}
+
+	public void onUpdateScreenOn(boolean screenOn) {
+		if (screenOn) {
+			getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		} else {
+			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		}
 	}
 }
--- app/src/main/java/com/cocolocomoco/tapalap/SettingsFragment.java
+++ app/src/main/java/com/cocolocomoco/tapalap/SettingsFragment.java
@@ -1,16 +1,13 @@
 package com.cocolocomoco.tapalap;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.view.WindowManager;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragment {
 
 	public SettingsFragment() {
 		// Required empty public constructor
@@ -22,40 +19,5 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 
 		// Load the preferences from an XML resource
 		addPreferencesFromResource(R.xml.preferences);
-
-		// Register listener for preference change
-		//PreferenceManager.getDefaultSharedPreferences(getActivity()).registerOnSharedPreferenceChangeListener(this);
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
-		// Register listener for preference change
-		getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-
-		// Unregister listener for preference change
-		getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-	}
-
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		if (key.equals(SettingsActivity.KEY_PREF_SCREEN_ON)) {
-			boolean result = sharedPreferences.getBoolean(key, true);
-			onUpdateScreenOn(result);
-		}
-	}
-
-	public void onUpdateScreenOn(boolean screenOn) {
-		if (screenOn) {
-			getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		} else {
-			getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		}
 	}
 }
