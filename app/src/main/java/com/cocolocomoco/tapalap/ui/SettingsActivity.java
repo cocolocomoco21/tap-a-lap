e83468ba326ed7a24f95fc5f101775c4592191a4
Matt Henricks <mchenricks@wisc.edu>
2017-07-29 01:22:58 -0500

    Implement lap per mile rate setting

--- app/src/main/java/com/cocolocomoco/tapalap/ui/SettingsActivity.java
+++ app/src/main/java/com/cocolocomoco/tapalap/ui/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
 	protected static final String KEY_PREF_SCREEN_ON = "pref_screen_on";
+	protected static final String KEY_PREF_LAPS_PER_MILE= "pref_laps_per_mile";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/cocolocomoco/tapalap/ui/SettingsFragment.java
+++ app/src/main/java/com/cocolocomoco/tapalap/ui/SettingsFragment.java
@@ -1,15 +1,19 @@
 package com.cocolocomoco.tapalap.ui;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
+import java.util.Map;
 
 import com.cocolocomoco.tapalap.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 	public SettingsFragment() {
 		// Required empty public constructor
@@ -21,5 +25,55 @@ public class SettingsFragment extends PreferenceFragment {
 
 		// Load the preferences from an XML resource
 		addPreferencesFromResource(R.xml.preferences);
+
+		EditTextPreference pref = (EditTextPreference) getPreferenceManager().findPreference(SettingsActivity.KEY_PREF_LAPS_PER_MILE);
+		updateLapPerMilePreferenceSummary(pref);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
+
+		// Register listener for preference change
+		sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+		// Update any EditTextPreferences to show value in summary
+		for (Map.Entry<String, ?> preferenceEntry : sharedPreferences.getAll().entrySet()) {
+			if (preferenceEntry.getClass().equals(EditTextPreference.class)) {
+				updateLapPerMilePreferenceSummary((EditTextPreference)preferenceEntry.getValue());
+			}
+		}
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		// Unregister listener for preference change
+		getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if (key.equals(SettingsActivity.KEY_PREF_LAPS_PER_MILE)) {
+			EditTextPreference pref = (EditTextPreference) getPreferenceManager().findPreference(SettingsActivity.KEY_PREF_LAPS_PER_MILE);
+			updateLapPerMilePreferenceSummary(pref);
+		}
+	}
+
+	/**
+	 * Update lap per mile preference summary on the Settings page according to the preference paramter.
+	 * @param pref - Preference parameter used to get text from and update the setting. If empty,
+	 *             set to default.
+	 */
+	private void updateLapPerMilePreferenceSummary(EditTextPreference pref) {
+		String text = pref.getText();
+		if (text.isEmpty()) {
+			pref.setSummary(R.string.laps_per_mile_rate_summary);
+		} else {
+			pref.setSummary(pref.getText());
+		}
 	}
 }
